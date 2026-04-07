2144fe2b27dcc736e098dbb8b5e54b6267de6082
sulik <suli.khaliullin@gmail.com>
2026-04-07 19:24:45 +0500

    completed homework 13

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path
 from Shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello_world),
-    path('data/', views.get_data),
+    path('todo/', views.hello_world, name='todo_list'),
+    re_path(r'^todo/(?P<task_id>\d+)/$', views.hello_world, name='todo_detail'),
+    re_path(r'^todo/category/(?P<category>\w+)/$', views.hello_world, name='todo_category'),
 ]

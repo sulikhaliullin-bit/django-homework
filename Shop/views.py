d24b2ccfc632aa04e0f8300a85eac7728a4a5099
sulik <suli.khaliullin@gmail.com>
2026-04-07 19:31:58 +0500

    completed homework 12: data filtering

--- Shop/views.py
+++ Shop/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse, JsonResponse
+from .models import Bb
 
 def hello_world(request):
-    return HttpResponse("<h1>Привет!</h1><p>Это строка с HTML тегами для практической работы.</p>")
+    return HttpResponse("<h1>Привет!</h1><p>Это строка с HTML тегами.</p>")
 
 def get_data(request):
     data = [x for x in range(1, 11)]
     return JsonResponse(data, safe=False)
+
+def filter_data(request):
+
+    results = Bb.objects.values('title', 'price').exclude(price__isnull=True).exclude(price=0)
+    return JsonResponse(list(results), safe=False)
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,7 +4,13 @@ from Shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('hello/', views.hello_world),
+
+    path('data/', views.get_data),
+
+    path('filter/', views.filter_data),
+
     path('todo/', views.hello_world, name='todo_list'),
     re_path(r'^todo/(?P<task_id>\d+)/$', views.hello_world, name='todo_detail'),
-    re_path(r'^todo/category/(?P<category>\w+)/$', views.hello_world, name='todo_category'),
 ]

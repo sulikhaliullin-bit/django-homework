4dd7ed8db7e0624fdc361d4b3e1fe4e7bbac6177
sulik <suli.khaliullin@gmail.com>
2026-04-26 12:54:39 +0500

    Выполнена практическая работа №22

--- Shop/views.py
+++ Shop/views.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 from .models import Bb, Rubric
 from .forms import BbForm
 
+
 class BbIndexView(ListView):
     model = Bb
     template_name = 'Shop/index.html'
@@ -14,9 +15,16 @@ class BbIndexView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['rubrics'] = Rubric.objects.all()
-        # Данные для задания №22
+
         context['test_list'] = ['django', 'python', 'web', 'framework']
         context['current_date'] = timezone.now()
+
+
+        context['user_name'] = 'sulik'
+        context['long_text'] = 'Это очень длинный текст, который мы обрежем с помощью фильтра truncatewords.'
+        context['price'] = 1500.50
+        context['items_count'] = 5
+
         return context
 
 class BbCreateView(CreateView):
@@ -28,6 +36,7 @@ class BbCreateView(CreateView):
         context = super().get_context_data(**kwargs)
         context['rubrics'] = Rubric.objects.all()
         return context
+
 
 class MyRedirectView(RedirectView):
     url = '/'

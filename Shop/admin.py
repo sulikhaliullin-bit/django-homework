7db15be844ddc13288b8cd9c848d4366692fd3ea
sulik <suli.khaliullin@gmail.com>
2026-05-09 20:52:43 +0500

    practical 6 - all models added

--- Shop/admin.py
+++ Shop/admin.py
@@ -1,3 +1,9 @@
+from django.contrib import admin
 from .models import Product, IceCream, IceCreamKiosk, Parent, Child, Person
 
+admin.site.register(Product)
+admin.site.register(IceCream)
+admin.site.register(IceCreamKiosk)
+admin.site.register(Parent)
+admin.site.register(Child)
 admin.site.register(Person)

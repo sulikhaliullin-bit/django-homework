bcb979eef817f53885b543dc9d0a4066727a7cae
sulik <suli.khaliullin@gmail.com>
2026-05-09 20:51:12 +0500

    practical 6 - Person model added

--- Shop/models.py
+++ Shop/models.py
@@ -5,4 +5,11 @@ class Person(models.Model):
     email = models.EmailField(blank=True)
 
     def __str__(self):
+        return self.name# Человек
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    email = models.EmailField(blank=True)
+
+    def __str__(self):
         return self.name

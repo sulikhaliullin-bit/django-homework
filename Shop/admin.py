c5b82f1a15eae5c9f4ddf9b478653e0ae12096f8
sulik <suli.khaliullin@gmail.com>
2026-05-09 20:49:10 +0500

    homework 6 - model relations ForeignKey

--- Shop/admin.py
+++ Shop/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, IceCream, IceCreamKiosk, Parent, Child
 
 admin.site.register(Product)
+admin.site.register(IceCream)
+admin.site.register(IceCreamKiosk)
+admin.site.register(Parent)
+admin.site.register(Child)
--- Shop/models.py
+++ Shop/models.py
@@ -3,10 +3,53 @@ from django.utils import timezone
 from django.core.exceptions import ValidationError
 
 
-# Валидатор — только положительные числа включая 0
 def validate_positive(value):
     if value < 0:
         raise ValidationError(f'{value} должно быть положительным числом или 0')
+
+
+# Киоск с мороженым
+class IceCreamKiosk(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+# Мороженое связано с киоском (многие к одному)
+class IceCream(models.Model):
+    kiosk = models.ForeignKey(IceCreamKiosk, on_delete=models.CASCADE, related_name='icecreams', null=True, blank=True)
+    name = models.CharField(max_length=200)
+    flavor = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[validate_positive])
+
+    def __str__(self):
+        return self.name
+
+    def get_id_and_flavor(self):
+        return f'{self.id} - {self.flavor}'
+
+    @classmethod
+    def get_total_price(cls):
+        return sum(i.price for i in cls.objects.all())
+
+
+# Родитель
+class Parent(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Child(models.Model):
+    parent = models.ForeignKey(Parent, on_delete=models.CASCADE, related_name='children')
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -18,29 +61,9 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-    # Метод возвращает id + name
     def get_id_and_name(self):
         return f'{self.id} - {self.name}'
 
-    # Метод возвращает сумму price всех продуктов
     @classmethod
     def get_total_price(cls):
         return sum(p.price for p in cls.objects.all())
-
-
-class IceCream(models.Model):
-    name = models.CharField(max_length=200)
-    flavor = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[validate_positive])
-
-    def __str__(self):
-        return self.name
-
-    # Метод возвращает id + flavor
-    def get_id_and_flavor(self):
-        return f'{self.id} - {self.flavor}'
-
-    # Метод возвращает сумму price всех мороженых
-    @classmethod
-    def get_total_price(cls):
-        return sum(i.price for i in cls.objects.all())
